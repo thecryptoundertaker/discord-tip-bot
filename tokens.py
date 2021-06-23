a992a8e4f6675d2a3772b0ed2d01c8f7f9e5a3d2
Tim Bart <barttim@protonmail.com>
2021-06-23 11:59:15 -0700

    add frozendict to reqs for immutable dictionaries, setup initial tokens

---
+++ tokens.py
@@ -0,0 +1,40 @@
+from frozendict import frozendict
+
+# supported tokens
+
+ftm = frozendict({
+    "contract_address": "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    "type": "ERC20",
+    "symbol": "WFTM",
+    "name": "Wrapped Fantom"
+    })
+
+fusd = frozendict({
+    "contract_address": "0xAd84341756Bf337f5a0164515b1f6F993D194E1f",
+    "type": "ERC20",
+    "symbol": "FUSD",
+    "name": "Fantom USD"
+    })
+
+
+
+tbond = frozendict({
+    "contract_address": "0x24248CD1747348bDC971a5395f4b3cd7feE94ea0",
+    "type": "ERC20",
+    "symbol": "TBOND",
+    "name": "Tomb Bond"
+    })
+
+tomb = frozendict({
+    "contract_address": "0x6c021Ae822BEa943b2E66552bDe1D2696a53fbB7",
+    "type": "ERC20",
+    "symbol": "TOMB",
+    "name": "Tomb"
+    })
+
+tshare = frozendict({
+    "contract_address": "0x4cdF39285D7Ca8eB3f090fDA0C069ba5F4145B37",
+    "type": "ERC20",
+    "symbol": "TSHARE",
+    "name": "Tomb Share"
+    })
