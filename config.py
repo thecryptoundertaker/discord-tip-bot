152fedae12bcf1b5873f123ce61af22e5d3a58d9
0xKalakaua <86123818+0xKalakaua@users.noreply.github.com>
2021-06-26 13:05:13 +0200

    Change discord setup from OOP to Functional programming

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 config = {
+        "LOGGER": True,
         "PROVIDER_ADDRESS": "wss://wsapi.fantom.network/",
         "PROVIDER_TYPE": "wss",
         "TOKEN": "your token here"
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from utils.fantom import connect_to_fantom
 from config import config
-from tipbot import tipbot
+from discord.ext import commands
+import logging
 
-# Globals
-# XXX these should be moved to a separate config file that also looks for envvars
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
-# PROVIDER_ADDRESS="wss://wsapi.fantom.network/"
-# PROVIDER_TYPE="wss"
+fantom = connect_to_fantom(config["PROVIDER_ADDRESS"])
+print(f"* Connected to Fantom!")
+print(f"* Node Address: {config['PROVIDER_ADDRESS']}")
+print(f"* Provider Type: {config['PROVIDER_TYPE']}")
+print(f"* Current Block: {fantom.eth.block_number}")
 
-def main():
-    fantom = connect_to_fantom(config["PROVIDER_ADDRESS"])
-    print(f"* Connected to Fantom!")
-    print(f"* Node Address: {config['PROVIDER_ADDRESS']}")
-    print(f"* Provider Type: {config['PROVIDER_TYPE']}")
-    print(f"* Current Block: {fantom.eth.block_number}")
-    bot = tipbot.TipBot(config)
-    bot.exec()
+bot = commands.Bot(command_prefix="$")
 
-if __name__ == "__main__":
-    main()
+@bot.event
+async def on_message(message):
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+@bot.event
+async def on_ready():
+    print(f"Bot {bot.user} is ready!")
+
+bot.run(config["TOKEN"])
