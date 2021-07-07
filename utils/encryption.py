c53fccdc6d312ea767922b46103f6d34e9f411c8
Tim Bart <barttim@protonmail.com>
2021-07-06 17:43:53 -0700

    use the pynacl builtin b64 encoders/decoders

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,5 +1,6 @@
 import nacl.secret
 import nacl.utils
+from nacl.encoding import URLSafeBase64Encoder
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 
 def generate_secret_key(key_size=nacl.secret.SecretBox.KEY_SIZE):
@@ -27,18 +28,22 @@ def decode_secret_key(key_string):
 
 def create_secret_box(key):
     try:
-        assert(type(key) is bytes)
-        assert(len(key) == 32)
-        box = nacl.secret.SecretBox(key)
+        box = nacl.secret.SecretBox(key, encoder=URLSafeBase64Encoder)
         return box
     except:
         raise
 
-def encrypt_data_with_box(box, data):
+def encrypt_data_in_box(box, data):
     try:
-        encrypted = box.encrypt(data)
+        encrypted = box.encrypt(data, encoder=URLSafeBase64Encoder)
         return encrypted
     except:
         raise
 
 
+def decrypt_data_in_box(box, data):
+    try:
+        decrypted = box.decrypt(data, encoder=URLSafeBase64Encoder)
+        return decrypted
+    except:
+        raise
